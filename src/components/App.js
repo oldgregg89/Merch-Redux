3a9a4a813de07c326d2de372d84ad3a8b0d2b0d5
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-09-09 10:01:24 -0700

    add react-strap and jumbotron to header
    Co-authored-by: Ian Gregg <iangregg188@gmail.com>
    Co-authored-by: jkbutserious <kim2jy@hotmail.com>
    Co-authored-by: hubba180 <kyle.james.hubbard@gmail.com>
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header'
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Container>
+        <Header />
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
